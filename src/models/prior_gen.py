86616fa878e040b8774985fa2ccd2149c954141d
rookie880 <s154993@student.dtu.dk>
2021-06-10 18:50:54 +0200

    many fixes. still issues with cholesky

--- src/models/prior_gen.py
+++ src/models/prior_gen.py
@@ -1,29 +1,35 @@
-#%% Libraries
-import cProfile
-from os import RTLD_NODELETE
-import numpy as np
-from numpy.lib.function_base import rot90
+# %% Libraries
+# import cProfile
+# from os import RTLD_NODELETE
+# import numpy as np
+# from numpy.lib.function_base import rot90
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
-from torch import random
-from torch.nn.modules.activation import ReLU
-from torch.overrides import get_overridable_functions
+# from torch import random
+# from torch.nn.modules.activation import ReLU
+# from torch.overrides import get_overridable_functions
 from torch.autograd import grad
-from torch.quantization.quantize import propagate_qconfig_
+# from torch.quantization.quantize import propagate_qconfig_
 import gpytorch
-torch.pi = torch.acos(torch.zeros(1)).item() * 2
-#import cProfile
-#import re
 
-#%% Functions
+torch.pi = torch.acos(torch.zeros(1)).item() * 2
+
+
+# import cProfile
+# import re
+
+# %% Functions
 class NN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.L1_fi = 1; self.L1_fo = 4
-        self.L2_fi = self.L1_fo; self.L2_fo = 4
-        self.L3_fi = self.L2_fo; self.L3_fo = 1
-        #self.l4_fi = self.L3_fo; self.l4_fo = 1
+        self.L1_fi = 1
+        self.L1_fo = 4
+        self.L2_fi = self.L1_fo
+        self.L2_fo = 4
+        self.L3_fi = self.L2_fo
+        self.L3_fo = 1
+        # self.l4_fi = self.L3_fo; self.l4_fo = 1
 
         self.structure = torch.tensor([[self.L1_fi, self.L1_fo], [self.L2_fi, self.L2_fo], [self.L3_fi, self.L3_fo]])
 
@@ -40,9 +46,13 @@ class NN(nn.Module):
         # self.A3 = nn.ReLU()
         # self.L4 = nn.Linear(self.l4_fi, self.l4_fo)
 
-
         # Hyperparameters
-        self.l = torch.sqrt(torch.tensor(0.1)); self.N = 50; self.sigma2 = 1; self.sigma2_n = 0.0001; self.sigma_prior = 1; self.total_no_param = 0
+        self.l = torch.sqrt(torch.tensor(0.1))
+        self.N = 50
+        self.sigma2 = 1
+        self.sigma2_n = 0.0001
+        self.sigma_prior = 1
+        self.total_no_param = 0
 
     def forward(self, x):
         x = self.L1(x)
@@ -50,51 +60,49 @@ class NN(nn.Module):
         x = self.L2(x)
         x = self.A2(x)
         x = self.L3(x)
-        #x = self.A3(x)
-        #u = self.L4(x)
+        # x = self.A3(x)
+        # u = self.L4(x)
         return x
 
     def prior(self):
-        theta_sample = torch.normal(torch.zeros(self.total_no_param), torch.ones(self.total_no_param)*self.sigma_prior)
+        theta_sample = torch.normal(torch.zeros(self.total_no_param),
+                                    torch.ones(self.total_no_param) * self.sigma_prior)
         self.update_param(theta_sample)
-        return 
+        return
 
     def get_total_no_param(self):
         theta_dict = self.state_dict()
-        theta = []
+        t1 = []
         for param_tensor in theta_dict:
-            temp = theta_dict[param_tensor]
-            theta.append(temp.view(-1))
-        theta = torch.cat(theta)
-        self.total_no_param = len(theta)
-        return 
+            t2 = theta_dict[param_tensor]
+            t1.append(t2.view(-1))
+        t1 = torch.cat(t1)
+        self.total_no_param = len(t1)
+        return
 
+    def energy_nn(self, x, theta_param):
+        u_latent = self.forward(x)
+        # temp = x - u # Bias toward M(x) = x = u
+        temp = u_latent  # Bias toward M(x) = 0
 
-    def U_NN(self, x, theta):
-        u = self.forward(x)
-        ll_NN = torch.tensor(0.0, requires_grad=True)
-        #pvect = x - u # Bias toward M(x) = x = u
-        pvect = u # Bias toward M(x) = 0
- 
-        ll_NN = torch.sum(pvect**2)/self.sigma_prior
-        lp_NN = torch.sum(theta**2)/self.sigma_prior
-        res = ll_NN + lp_NN
-        return res, u
+        ll_NN = torch.sum(temp ** 2) / self.sigma_prior
+        lp_NN = torch.sum(theta_param ** 2) / self.sigma_prior
+        return (ll_NN + lp_NN), u_latent
 
-    def update_param(self, theta):
+    def update_param(self, theta_param):
         theta_dict = self.state_dict()
         c = 0
         for param_tensor in theta_dict:
             s = theta_dict[param_tensor].size()
             n = s.numel()
-            param = torch.reshape(theta[c:c+n], s)
+            param = torch.reshape(theta_param[c:c + n], s)
             c = c + n
             theta_dict[param_tensor] = param
         self.load_state_dict(theta_dict)
 
-    def grad_calc(self, U):
+    def grad_calc(self, energy):
         self.zero_grad()
-        temp = grad(U, self.parameters())
+        temp = grad(energy, self.parameters())
         grads = []
         for g in temp:
             grads.append(g.view(-1))
@@ -103,14 +111,15 @@ class NN(nn.Module):
 
     def get_param(self):
         theta_dict = self.state_dict()
-        theta = []
+        out = []
         for param_tensor in theta_dict:
             temp = theta_dict[param_tensor]
-            theta.append(temp.view(-1))
-        theta = torch.cat(theta)
-        return theta
+            out.append(temp.view(-1))
+        out = torch.cat(out)
+        return out
 
-def SE_kern(u, l, sigma2, N):
+
+def se_kern(u_kern, l, sigma2, N):
     # K = torch.zeros([N,N])
     # l = 2*l*l
     # for i in range(N):
@@ -118,64 +127,69 @@ def SE_kern(u, l, sigma2, N):
     #         temp = sigma2*torch.exp(-(x[i] - x[j])**2/l)
     #         K[i,j] = temp
     #         K[j,i] = temp
-    temp = K_module(u)
-    K = temp.evaluate()
-    return K
+    temp = K_module(u_kern)
+    out = temp.evaluate()
+    return out
 
-def GP_prior(u, l, sigma2, sigma2_n , N):
-    K = SE_kern(u, l, sigma2, N)
-    B = K + sigma2_n*torch.eye(N)
-    f = torch.distributions.MultivariateNormal(torch.zeros(N), B)
-    f = f.sample()
-    #f = torch.normal(torch.zeros(N), B)
-    omega = torch.normal(torch.zeros(N), torch.ones(N)*sigma2_n)
-    y = f + omega
-    y = torch.reshape(y, (N, 1))
-    return y
 
-def GP_pred(mod, x, y):
-    u = mod.forward(x)
-    K = SE_kern(u, mod.l, mod.sigma2, mod.N) #l = 2*mod.l*mod.l #K = mod.sigma2*torch.exp(-torch.cdist(u,u, p = 2)**2/l)
+def gp_prior(mod, u_prior):
+    K_prior = se_kern(u_prior, mod.l, mod.sigma2, mod.N)
+    B = K_prior + mod.sigma2_n * torch.eye(mod.N)
+    f_prior = torch.distributions.MultivariateNormal(torch.zeros(mod.N), B)
+    f_prior = f_prior.sample()
+    omega = torch.normal(torch.zeros(mod.N), torch.ones(mod.N) * mod.sigma2_n)
+    out = f_prior + omega
+    out = torch.reshape(out, (mod.N, 1))
+    return out
 
-    B = K + mod.sigma2_n*torch.eye(mod.N)
-    L = torch.cholesky(B)
-    alpha = torch.cholesky_solve(y,L)
 
-    f = K @ alpha
+def gp_pred(mod, x_pred, y_pred):
+    u_pred = mod.forward(x_pred)
+    K_pred = se_kern(u_pred, mod.l, mod.sigma2, mod.N)
+    # l = 2*mod.l*mod.l #K = mod.sigma2*torch.exp(-torch.cdist(u,u, p = 2)**2/l)
 
-    ym = y - f
-    LL_GP = torch.sum(ym**2)/mod.sigma2_n
+    B = K_pred + mod.sigma2_n * torch.eye(mod.N)
+    L_pred = torch.cholesky(B)
+    alpha_pred = torch.cholesky_solve(y_pred, L_pred)
+
+    f_pred = K_pred @ alpha_pred
+
+    ym = y_pred - f_pred
+    LL_GP = torch.sum(ym ** 2) / mod.sigma2_n
 
     # Prior
-    L  = torch.cholesky(K + 1e-3*torch.eye(mod.N))
-    t1 = torch.cholesky_solve(f, L)
-    LP_GP = t1.t() @ f + torch.sum(torch.log(torch.diag(L)))
+    L_pred = torch.cholesky(K_pred + 1e-3 * torch.eye(mod.N))
+    t1 = torch.cholesky_solve(f_pred, L_pred)
+    LP_GP = t1.t() @ f_pred + torch.sum(torch.log(torch.diag(L_pred)))
 
-    U_GP = LL_GP + LP_GP
-    return f, U_GP
+    out = LL_GP + LP_GP
+    return f_pred, out
 
-def M_spline_func(t1, t2, k, x, N):
-    u = torch.zeros((N,1), requires_grad=True) # M(x) = u
+
+def m_spline_func(t1, t2, k, x, N):
+    out = torch.zeros((N, 1), requires_grad=True)  # M(x) = u
     for i in range(N):
         if x[i] < t1:
-            u[i] = k/(t1-min(x))*x[i] - k/(t1-min(x))*min(x)
-        elif x[i] >= t1 and x[i] < t2:
-            u[i] = k
+            out[i] = k / (t1 - min(x)) * x[i] - k / (t1 - min(x)) * min(x)
+        elif t1 <= x[i] < t2:
+            out[i] = k
         else:
-            u[i] = k/(max(x)-t2)*x[i] - k/(max(x)-t2)*t2  + k 
-    return u
+            out[i] = k / (max(x) - t2) * x[i] - k / (max(x) - t2) * t2 + k
+    return out
 
-def M_square_func(t, x, N):
-    u = torch.zeros((N,1))
+
+def m_square_func(threshold, x, N):
+    out = torch.zeros((N, 1))
     for i in range(N):
-        if x[i] < t:
-            u[i] = 1
+        if x[i] < threshold:
+            out[i] = 1
         else:
-            u[i] = 0
-    return u
+            out[i] = 0
+    return out
 
-def gaussian_mixture_2(m,sigma2,N):
-    x = torch.zeros((N,1))
+
+def gaussian_mixture_2(m, sigma2, N):
+    x = torch.zeros((N, 1))
     for i in range(N):
         if torch.rand(1) < 0.5:
             x[i] = torch.normal(m, torch.sqrt(torch.tensor(sigma2)))
@@ -184,7 +198,7 @@ def gaussian_mixture_2(m,sigma2,N):
     return x
 
 
-#%%
+# %%
 net = NN()
 net.get_total_no_param()
 net.prior()
@@ -195,13 +209,14 @@ K_module = gpytorch.kernels.RBFKernel()
 K_module.lengthscale = net.l
 K_module.outputscale = net.sigma2
 
-## Generate Data and Plot ##
-x_space = gaussian_mixture_2(4, 2.0, net.N) #x_space = torch.linspace(-5, 5, net.N)#x_space = torch.reshape(x_space, (net.N,1))
-u = M_square_func(0, x_space, net.N) #u = M_func(-2,2,2, x_space, net.N) #u = net.forward(x_space)
-y = GP_prior(u, net.l, net.sigma2, net.sigma2_n, net.N)
+# Generate Data and Plot ##
+x_space = gaussian_mixture_2(4, 2.0, net.N)
+# x_space = torch.linspace(-5, 5, net.N)#x_space = torch.reshape(x_space, (net.N,1))
+u = m_square_func(0, x_space, net.N)
+# u = M_func(-2,2,2, x_space, net.N) #u = net.forward(x_space)
+y = gp_prior(net, u)
 
-## Plot data ##
-# Latent space
+# Plot data #
 plt.scatter(x_space, u.data, 20, 'r')
 plt.show()
 # Observations
@@ -209,89 +224,89 @@ plt.scatter(x_space, y, 20, 'b')
 plt.legend(['M(x)=u', 'y=GP(u)'])
 plt.show()
 
-
-T = torch.tensor(200000)
-S = 0 # Number of samples
-L = 5 # L = 3 ep = 0.0001 works (but it is to low for sure?.?)
+# %%
+T = 1000
+S = 0  # Number of samples
+L = 5  # L = 3 ep = 0.0001 works (but it is to low for sure?.?)
 ep0 = 0.01
-M = torch.tensor(50)# Number of cycles
-ep_space = ep0*0.5*(torch.cos(torch.pi*torch.fmod(torch.linspace(0, T, T), torch.ceil(T/M))/torch.ceil(T/M))+1)
+M = 50  # Number of cycles
+ep_space = ep0 * 0.5 * (torch.cos(
+    torch.pi * torch.fmod(torch.linspace(0, T, T), torch.ceil(torch.tensor(T) / torch.tensor(M))) / torch.ceil(
+        torch.tensor(T) / torch.tensor(M))) + 1)
 plt.plot(ep_space)
 plt.show()
 
-fm = torch.zeros((net.N,1))
-um = torch.zeros((net.N,1))
+fm = torch.zeros((net.N, 1))
+um = torch.zeros((net.N, 1))
+G = torch.zeros(T)
 
 # Init
 net.prior()
 theta = net.get_param()
-U_NN, ign = net.U_NN(x_space, theta)
-f, U_GP = GP_pred(net, x_space, y)
+U_NN, ign = net.energy_nn(x_space, theta)
+f, U_GP = gp_pred(net, x_space, y)
 U = U_GP + U_NN
-S = 0 # Number of samples
 um_samples = []
 grad_U = net.grad_calc(U)
-fm = torch.zeros((net.N,1))
-um = torch.zeros((net.N,1))
-G = torch.zeros(T)
 
-#%% HMCMC
+# H-MCMC
 for t in range(T):
     rp = torch.normal(torch.zeros(net.total_no_param), torch.ones(net.total_no_param))
-    r = rp 
+    r = rp
     theta_p = theta
     grad_U_p = grad_U
     U_p = U
 
     # Leapfrog
-    #ep = torch.rand(1)*0.01
+    # ep = torch.rand(1)*0.01
     ep = ep_space[t]
     for i in range(L):
-        rp = rp - ep*grad_U_p*0.5
-        theta_p = theta_p + ep*rp
+        rp = rp - ep * grad_U_p * 0.5
+        theta_p = theta_p + ep * rp
 
         # Calculate gradient of U_p
         net.update_param(theta_p)
-        U_NN_p, up = net.U_NN(x_space, theta_p) # NN pot. Energy
-        f_p, U_GP_p = GP_pred(net, x_space, y) # GP Pot. Energy
-        U_p = U_GP_p + U_NN_p # Proposed Pot. Energy
+        U_NN_p, up = net.energy_nn(x_space, theta_p)  # NN pot. Energy
+        f_p, U_GP_p = gp_pred(net, x_space, y)  # GP Pot. Energy
+        U_p = U_GP_p + U_NN_p  # Proposed Pot. Energy
         grad_U_p = net.grad_calc(U_p)
-        rp = rp - ep*grad_U_p*0.5
+        rp = rp - ep * grad_U_p * 0.5
 
-    G[t] = torch.sqrt(torch.sum(grad_U_p**2))/net.total_no_param # Normalizes the gradient with the number of parameters to be sampled
-    K = torch.sum(r**2)/2
-    Kp = torch.sum(rp**2)/2
+    G[t] = torch.sqrt(torch.sum(
+        grad_U_p ** 2)) / net.total_no_param  # Normalizes the gradient with the number of parameters to be sampled
+    K = torch.sum(r ** 2) / 2
+    Kp = torch.sum(rp ** 2) / 2
     alpha = -U_p - Kp + U + K
 
     # print("Kp: ", Kp)
     # print("K: ", K)
     # print("Grad Norm: ", G[t] )
     # print("Ep: ", ep)
-    
+
     if torch.log(torch.rand(1)) < alpha:
         theta = theta_p
         net.update_param(theta)
-        U_NN = U_NN_p ## Neural Network potential energy update
-        U_GP = U_GP_p  ## GP potential Energy update
-        U = U_p ## Potential Energy Update
+        U_NN = U_NN_p  # Neural Network potential energy update
+        U_GP = U_GP_p  # GP potential Energy update
+        U = U_p  # Potential Energy Update
 
-        #print('Sample')
+        # print('Sample')
         if t > 100:
             fm = fm + f_p
-            torch.cat((um_samples, up))
+            #torch.cat((um_samples, up))
             um = um + up
             S = S + 1
     else:
         net.update_param(theta)
-        #print('No Sample')
+        # print('No Sample')
     print(t)
     print(S)
-    #cProfile.run('re.compile("foo|bar")')
+    # cProfile.run('re.compile("foo|bar")')
 
-#%% 
+# %%
 # Average over samples
-f_mean = fm/(S)
-u_mean = um/(S)
+f_mean = fm / S
+u_mean = um / S
 
 # Plot
 # Latent space
@@ -307,7 +322,7 @@ plt.ylabel('u')
 plt.show()
 
 # Targets
-plt.scatter(x_space, y )
+plt.scatter(x_space, y)
 plt.scatter(x_space, f_mean.data)
 plt.legend(['y', 'GP(u) = ybar', 'GP(uhat) = yhat'])
 
@@ -316,9 +331,7 @@ plt.ylabel('y')
 plt.show()
 
 plt.plot(G)
-plt.title(('||Grad_U_p||_2.',' ep = ',str(ep), 'L = ', str(L)))
 plt.show()
-
 
 '''
 L = 5, ep1 = 0.0007, ep2 = 0.0005
